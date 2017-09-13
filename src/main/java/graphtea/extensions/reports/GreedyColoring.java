c57ef62a10cde2a35129ac49dcddda0df7e2347d
Ali Rostami <rostamiev@gmail.com>
2017-09-13 09:27:52 +0200

    small changes

--- src/main/java/graphtea/extensions/reports/GreedyColoring.java
+++ src/main/java/graphtea/extensions/reports/GreedyColoring.java
@@ -30,55 +30,53 @@ public class GreedyColoring implements GraphReportExtension {
     }
 
     public Object calculate(GraphModel g) {
-        int V = g.numOfVertices();
-        int result[] = new int[V];
-        result[0]  = 0;
+        int numOfVertices = g.numOfVertices();
+        int mapToColors[] = new int[numOfVertices];
+        boolean available[] = new boolean[numOfVertices];
+        mapToColors[0] = 0;
 
-        for (int u = 1; u < V; u++)
-            result[u] = -1;
+        for (int u = 1; u < numOfVertices; u++)
+            mapToColors[u] = -1;
 
-        boolean available[] = new boolean[V];
-        for (int cr = 0; cr < V; cr++)
+        for (int cr = 0; cr < numOfVertices; cr++)
             available[cr] = false;
 
-        for (int u = 1; u < V; u++) {
+        for (int u = 1; u < numOfVertices; u++) {
 
-            Iterator<Vertex> it = g.directNeighbors(g.getVertex(u)).iterator() ;
-            while (it.hasNext())
-            {
+            Iterator<Vertex> it = g.directNeighbors(g.getVertex(u)).iterator();
+            while (it.hasNext()) {
                 int i = it.next().getId();
-                if (result[i] != -1)
-                    available[result[i]] = true;
+                if (mapToColors[i] != -1)
+                    available[mapToColors[i]] = true;
             }
 
-            int cr;
-            for (cr = 0; cr < V; cr++)
-                if (available[cr] == false)
+            for (int color = 0; color < numOfVertices; color++)
+                if (available[color] == false) {
+                    mapToColors[u] = color;
                     break;
+                }
 
-            result[u] = cr;
 
-            it = g.directNeighbors(g.getVertex(u)).iterator() ;
-            while (it.hasNext())
-            {
+            it = g.directNeighbors(g.getVertex(u)).iterator();
+            while (it.hasNext()) {
                 int i = it.next().getId();
-                if (result[i] != -1)
-                    available[result[i]] = false;
+                if (mapToColors[i] != -1)
+                    available[mapToColors[i]] = false;
             }
         }
 
         int max = 0;
-        for(int i : result) {
-            if(max < i) max = i;
+        for (int i : mapToColors) {
+            if (max < i) max = i;
         }
         max++;
 
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("num_of_colors",max);
+            jsonObject.put("num_of_colors", max);
             JSONArray jsonArray = new JSONArray();
-            for(int i =0; i < result.length;i++) jsonArray.put(result[i]);
-            jsonObject.put("colors",jsonArray);
+            for (int i = 0; i < mapToColors.length; i++) jsonArray.put(mapToColors[i]);
+            jsonObject.put("colors", jsonArray);
         } catch (JSONException e) {
             e.printStackTrace();
         }
