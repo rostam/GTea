9b2b858393a10b8792a046ab429f77fa08912486
AlexCre <a.cregten@gmail.com>
2017-10-12 14:25:03 +0000

    Some refactoring and base code for reduce, T5, Q2

--- src/main/java/graphtea/extensions/reports/planarity/planaritypq/PQ.java
+++ src/main/java/graphtea/extensions/reports/planarity/planaritypq/PQ.java
@@ -1,68 +1,15 @@
 package graphtea.extensions.reports.planarity.planaritypq;
 
-import graphtea.graph.graph.Edge;
-import graphtea.graph.graph.GraphModel;
-import graphtea.graph.graph.Vertex;
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PQ {
     private int blockCount;
     private int blockedNodes;
     private int offTheTop;
 
-    public boolean planar(GraphModel graph){
-        graph.setDirected(false); // do I need this?
-
-        Iterable<Edge> source = graph.getEdges();
-        ArrayList<PQEdge> U = new ArrayList<>();
-        for(Edge e : source){
-
-            U.add(new PQEdge(e, (PQNode) e.source, (PQNode) e.target));
-        }
-        PQTree T = new PQTree(U, U);
-
-        for(int j=2; j<graph.getVertexArray().length-1; j++){
-            Vertex curVertex = graph.getVertex(j);
-
-            // S := the set of edges whose higher-numbered vertex is j
-            List<PQEdge> S = new ArrayList<>();
-            for(Vertex v : graph.directNeighbors(curVertex)){
-                S.add((PQEdge) graph.getEdge(curVertex, v));
-            }
-
-            T = bubble(T, S);
-            T = reduce(T, S);
-
-            if(T == null){
-                return false;
-            }
-
-            // S' := the set of edges whose lower-numbered vertex is j
-            List<PQEdge> SPrime = new ArrayList<>();
-            for(Vertex v : graph.directNeighbors(curVertex)){
-                S.add((PQEdge) graph.getEdge(v, curVertex));
-            }
-
-            PQNode root = root(T,S);
-            if(root.type() == PQNode.QNODE){
-                // Replace the full children of root(T,S) and their descendants by T(S',S')
-                replace(root, new PQTree(SPrime, SPrime));
-            } else {
-                // Replace root(T,S) and its descendants by T(S',S')
-                replace(root, new PQTree(SPrime, SPrime));
-            }
-
-            // U = U - S union S'
-            U.removeAll(union(S, SPrime));
-        }
-
-        return true;
-    }
-
-
-    public PQTree bubble(PQTree T, List<PQEdge> S){
-        Queue<PQEdge> queue = new LinkedList<PQEdge>(S);
+    public PQTree bubble(PQTree T, List<PQNode> S){
+        Queue<PQNode> queue = new LinkedList<>(S);
         blockCount = 0;
         blockedNodes = 0;
         offTheTop = 0;
@@ -76,7 +23,6 @@ public class PQ {
             PQNode x = queue.remove().parent;
             x.blocked = true;
 
-            /** Should we have PQEdge/PQNode or just PQNode.. ?? Decide!*/
             List<PQNode> BS = new ArrayList<>();
             List<PQNode> US = new ArrayList<>();
             for(PQNode n : T.immediateSiblings(x)){
@@ -96,37 +42,324 @@ public class PQ {
                 x.blocked = false;
             }
 
+            int listSize = 0;
             if(!x.blocked){
                 PQNode y = x.parent;
                 if(BS.size() > 0){
-                    // TODO: LIST := the maximal consecutive set of blocked siblings adjacent to x
-
-
+                    // list := the maximal consecutive set of blocked siblings adjacent to x
+                    List<PQNode> list = x.siblings.stream().filter(u -> u.blocked).collect(Collectors.toList());
+                    listSize = list.size();
+                    for(PQNode z : list){
+                        z.blocked = false;
+                        y.pertinentChildCount++;
+                    }
                 }
+                if(y == null){
+                    offTheTop = 1;
+                }
+                else {
+                    y.pertinentChildCount++;
+                    if(!y.marked){
+                        queue.add(y);
+                        y.queued = true;
+                    }
+                }
+                blockCount = blockCount - BS.size();
+                blockedNodes = blockedNodes - listSize;
+            }
+            else {
+                blockCount = blockCount + 1 - BS.size();
+                blockedNodes = blockedNodes + 1;
             }
 
         }
 
-        return null;
+        return T;
     }
 
-    public PQTree reduce(PQTree T, List<PQEdge> S){
+    public PQTree reduce(PQTree T, List<PQNode> S){
+        Queue<PQNode> queue = new LinkedList<>(S);
+        for(PQNode x : S){
+            x.pertinentLeafCount = 1;
+        }
+        while(queue.size() > 0){
+            PQNode x = queue.remove();
+            if(x.pertinentLeafCount < S.size()){
+                // X is not ROOT(T, S)
 
-        return null;
+                PQNode y = x.parent;
+                y.pertinentLeafCount = y.pertinentLeafCount + x.pertinentLeafCount;
+                y.pertinentChildCount = y.pertinentChildCount - 1;
+                if(y.pertinentChildCount == 0){
+                    queue.add(y);
+                }
+                if(TEMPLATE_L1(x)) break;
+                if(TEMPLATE_P1(x)) break;
+                if(TEMPLATE_P3(x)) break;
+                if(TEMPLATE_P5(x)) break;
+                if(TEMPLATE_Q1(x)) break;
+                if(TEMPLATE_Q2(x)) break;
+
+                // If all templates fail, return null tree
+                T = null;
+                return T;
+            }
+            else {
+                // X is ROOT(T, S)
+
+                if(TEMPLATE_L1(x)) break;
+                if(TEMPLATE_P1(x)) break;
+                if(TEMPLATE_P2(x)) break;
+                if(TEMPLATE_P4(x)) break;
+                if(TEMPLATE_P6(x)) break;
+                if(TEMPLATE_Q1(x)) break;
+                if(TEMPLATE_Q2(x)) break;
+                if(TEMPLATE_Q3(x)) break;
+
+                // If all templates fail, return null tree
+                T = null;
+                return T;
+            }
+
+        }
+
+        return T;
     }
 
     public void replace(PQNode T, PQTree TPrime){
 
     }
 
-    public PQNode root(PQTree T, List<PQEdge> S){
+    public PQNode root(PQTree T, List<PQNode> S){
 
         return null;
     }
 
-    public static List<PQEdge> union(List<PQEdge> list1, List<PQEdge> list2) {
-        HashSet<PQEdge> set = new HashSet<>();
+    /**
+    * TEMPLATES
+    * */
 
+    public boolean TEMPLATE_L1(PQNode x){
+        return false;
+    }
+
+    public boolean TEMPLATE_P1(PQNode x){
+        return false;
+    }
+    public boolean TEMPLATE_P2(PQNode x){
+        return false;
+    }
+    public boolean TEMPLATE_P3(PQNode x){
+        return false;
+    }
+    public boolean TEMPLATE_P4(PQNode x){
+        return false;
+    }
+
+    public boolean TEMPLATE_P5(PQNode x){
+
+        if(x.nodeType(PQNode.PNODE)){
+            return false;
+        }
+        if(x.partialChildren().size() != 1){
+            return false;
+        }
+
+        // TODO: Y := the unique element in PARTIAL_CHILDREN(X)
+        PQNode y = x.partialChildren().get(0);
+        // TODO: EC := the unique element in ENDMOST_CHILDREN(Y) labeled "empty"
+        PQNode ec = x.endmostChildren().get(0);
+        // TODO: FC := the unique element in ENDMOST_CHILDREN(Y) labeled "full"
+        PQNode fc = x.endmostChildren().get(0);
+
+        // The following statement may be performed in time on the order of the number of
+        // pertinent children of X through the use of the CIRCULAR_LINK fields
+
+        PQNode es = null;
+        if(y.emptySibling() != null){
+            es = y.emptySibling();
+        }
+
+        // Y will be the root of the replacement
+
+        y.parent = x.parent;
+        y.pertinentLeafCount = x.pertinentLeafCount;
+        y.label = PQNode.PARTIAL;
+        y.parent.setPartialChildren( union(y.parent.partialChildren(), Arrays.asList(y)) );
+
+        // Remove Y from the list of children of X formed by the CIRCULAR_LINK fields
+        y.removeFromCircularLink();
+
+        if(x.immediateSiblings().size() == 0){
+            x.parent.replaceInCircularLink(x, y);
+        }
+        else {
+            x.parent.replaceInImmediateSiblings(x, y);
+            if(y.parent.endmostChildren().contains(x)) {
+                y.parent.endmostChildren().removeAll(union(Arrays.asList(x), Arrays.asList(y)));
+            }
+        }
+
+        if(x.fullChildren().size() > 0){
+            PQNode zf = null;
+            if(x.fullChildren().size() == 1){
+                // TODO: Let ZF be the unique element in FULL_CHILDREN(X)
+                zf = x.fullChildren().get(0);
+                // Remove zf from the CIRCULAR_LINK list of which it is currently a member
+                zf.removeFromCircularLink();
+            }
+            else {
+                zf = new PQNode();
+                zf.nodeType = PQNode.PNODE;
+                zf.label = PQNode.FULL;
+                for(PQNode w : x.fullChildren()){
+                    w.removeFromCircularLink();
+                    w.parent = zf;
+                }
+                // Set the CIRCULAR_LINK fields of the nodes in FULL_CHILDREN(X) to form a doubly-linked circular list
+                // - Possibly move this to PQNode class
+                PQNode startN = x.fullChildren().get(0);
+                PQNode endN = x.fullChildren().get(x.fullChildren().size()-1);
+                startN.circularLink_prev = endN;
+                startN.circularLink_next = x.fullChildren().get(1);
+                endN.circularLink_next = startN;
+                endN.circularLink_prev = x.fullChildren().get(x.fullChildren().size()-2);
+                for(int i=1; i<x.fullChildren().size()-2; i++){
+                    PQNode w = x.fullChildren().get(i);
+                    w.circularLink_prev = x.fullChildren().get(i-1);
+                    w.circularLink_next = x.fullChildren().get(i+1);
+                }
+
+                zf.childCount = x.fullChildren().size();
+            }
+
+            zf.parent = y;
+            fc.setImmediateSiblings(union(fc.immediateSiblings(), Arrays.asList(zf)));
+            zf.setImmediateSiblings(Arrays.asList(fc));
+            y.endmostChildren().removeAll(union(Arrays.asList(fc), Arrays.asList(zf)));
+        }
+
+        int numberEmpty = x.childCount - x.fullChildren().size() - x.partialChildren().size();
+        if(numberEmpty > 0){
+            PQNode ze;
+            if(numberEmpty == 1){
+                ze = es; // Could be null (?)
+            }
+            else{
+                ze = x;
+                ze.label = PQNode.EMPTY;
+                ze.childCount = numberEmpty;
+            }
+            ze.parent = y;
+            ec.setImmediateSiblings(union(ec.immediateSiblings(), Arrays.asList(ze)));
+            ze.setImmediateSiblings(Arrays.asList(ec));
+            y.endmostChildren().removeAll(union(Arrays.asList(ec), Arrays.asList(ze)));
+        }
+
+        if(numberEmpty < 2){
+            x = null;
+        }
+        return true;
+    }
+    public boolean TEMPLATE_P6(PQNode x){
+        return false;
+    }
+
+    public boolean TEMPLATE_Q1(PQNode x){
+        return false;
+    }
+
+    public boolean TEMPLATE_Q2(PQNode x){
+
+        if(!x.nodeType(PQNode.QNODE)){
+            return false;
+        }
+        if(x.nodeType(PQNode.PSEUDO_NODE)){
+            return false;
+        }
+
+        if(x.fullChildren().size() > 0) {
+
+            List<PQNode> xChildren = intersection(x.fullChildren(), x.endmostChildren());
+            if (xChildren.size() != 1) {
+                return false;
+            }
+            // let Y be the unique element in FULL_CHILDREN(X) union ENDMOST_CHILDREN(X)
+            PQNode y = union(x.fullChildren(), x.endmostChildren()).get(0);
+
+            for(int i=0; i<x.fullChildren().size(); i++){
+                if(x.fullChildren().contains(y)){
+                    return false;
+                }
+                else {
+                    // Y := the next sibling in the chain of children of X
+                    y = y.circularLink_next;
+                }
+            }
+
+            if( !subset(x.partialChildren(), Arrays.asList(y)) ){
+                return false;
+            }
+
+        }
+        else if(!subset(x.partialChildren(), x.endmostChildren())){
+            return false;
+        }
+
+        x.label = PQNode.PARTIAL;
+        x.parent.setPartialChildren(union(x.parent.partialChildren(), Arrays.asList(x)));
+
+        if(x.partialChildren().size() > 0){
+            // TODO: Y := the unique element of PARTIAL_CHILDREN(X)
+            PQNode y = x.partialChildren().get(0);
+
+            PQNode fc = y.endmostChild(PQNode.FULL);
+
+            PQNode yFullSibling = y.getImmediateSiblingOfNodeType(PQNode.FULL);
+            if(yFullSibling != null){
+                PQNode fs = yFullSibling;
+                fs.immediateSiblings().removeAll(union(Arrays.asList(y), Arrays.asList(fc)));
+                fc.setImmediateSiblings(union(fc.immediateSiblings(), Arrays.asList(fs)));
+            }
+            else {
+                x.endmostChildren().removeAll(union(Arrays.asList(y), Arrays.asList(fc)));
+                fc.parent = x;
+            }
+
+            PQNode ec =  y.endmostChild(PQNode.EMPTY);
+            PQNode yEmptyImmSibling = y.getImmediateSiblingOfNodeType(PQNode.EMPTY);
+            if(yEmptyImmSibling != null){
+                PQNode es = yEmptyImmSibling;
+                es.immediateSiblings().removeAll(Arrays.asList(y, ec));
+                ec.setImmediateSiblings(union(ec.immediateSiblings(), Arrays.asList(es)));
+            }
+            else {
+                x.endmostChildren().removeAll(Arrays.asList(y, ec));
+                ec.parent = x;
+            }
+            y = null;
+        }
+
+        return true;
+    }
+    public boolean TEMPLATE_Q3(PQNode x){
+        return false;
+    }
+
+    public static <E> boolean subset(List<E> list1, List<E> list2) {
+        return list2.containsAll(list1);
+    }
+
+    public static <E> List<E> intersection(List<E> list1, List<E> list2){
+        list1.retainAll(list2);
+        return list1;
+    }
+
+
+    public static <E> List<E> union(List<E> list1, List<E> list2) {
+        HashSet<E> set = new HashSet<>();
+
+        // set does not insert duplicates
         set.addAll(list1);
         set.addAll(list2);
 
@@ -134,3 +367,18 @@ public class PQ {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/graphtea/extensions/reports/planarity/planaritypq/PQNode.java
+++ src/main/java/graphtea/extensions/reports/planarity/planaritypq/PQNode.java
@@ -7,21 +7,103 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PQNode extends Vertex {
-    static int PNODE = 0;
-    static int QNODE = 1;
-    static int ELEMENT = 2;
+    static String PNODE = "p-node";
+    static String QNODE = "q-node";
+    static String PSEUDO_NODE = "pseudonode";
+    String nodeType = ""; // update the type
+
+    static String PARTIAL = "partial";
+    static String FULL = "full";
+    static String EMPTY = "EMPTY";
+    String label = "";
+
+    PQNode circularLink_next;
+    PQNode circularLink_prev;
+
+    int pertinentChildCount;
+
+    int pertinentLeafCount;
+
+    boolean queued;
+
     boolean blocked;
+
     boolean marked;
+
+    int childCount;
+
     PQNode parent;
-    List<Object> siblings = new ArrayList<Object>();
+    List<PQNode> siblings = new ArrayList<>();
 
     public PQNode(){
         blocked = false;
         marked = false;
+        pertinentChildCount = 0;
+        pertinentLeafCount = 0;
+        queued = false;
+        circularLink_next = null;
+        circularLink_prev = null;
+        childCount = 0;
     }
 
-    public int type(){
-        return -1;
+    public boolean nodeType(String checkType){
+        return nodeType.equals(checkType);
     }
+
+    public List<PQNode> partialChildren(){
+        return null;
+    }
+
+    public void setPartialChildren(List<PQNode> list){
+
+    }
+
+    public List<PQNode> endmostChildren(){
+        return null;
+    }
+
+    public PQNode emptySibling(){
+        return null;
+    }
+
+    public void removeFromCircularLink(){
+        this.circularLink_next.circularLink_prev = this.circularLink_prev;
+        this.circularLink_prev.circularLink_next = this.circularLink_next;
+        this.circularLink_next = null;
+        this.circularLink_prev = null;
+    }
+
+    public List<PQNode> immediateSiblings(){
+        return null;
+    }
+
+    public void replaceInCircularLink(PQNode x, PQNode y){
+        // x is replaced by y
+    }
+
+    public void replaceInImmediateSiblings(PQNode x, PQNode y){
+        // x is replaced by y
+    }
+
+    public void setEndmostChildren(List<PQNode> list){
+
+    }
+
+    public List<PQNode> fullChildren(){
+        return null;
+    }
+
+    public void setImmediateSiblings(List<PQNode> list){
+
+    }
+
+    public PQNode getImmediateSiblingOfNodeType(String nodeType){
+        return null;
+    }
+
+    public PQNode endmostChild(String nodeType){
+        return null;
+    }
+
 
 }
